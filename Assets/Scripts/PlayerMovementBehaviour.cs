c12ac34da7e9ba9c6752ab4c07342ffe328a8f99
OnatKorucu <onatkorucu@gmail.com>
2022-04-29 01:29:03 +0300

    feat: gameplay environment created, player movement added.

--- Assets/Scripts/PlayerMovementBehaviour.cs
+++ Assets/Scripts/PlayerMovementBehaviour.cs
@@ -1,37 +1,69 @@
 using UnityEngine;
         
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerMovementBehaviour : MonoBehaviour
 {
+    #region Variables
+    [SerializeField] [Range(0f, 1f)] private float speedModifier;
+    [SerializeField] [Range(0f, 100f)] private float maxVelocity;
+
+    private Rigidbody rigidbody;
+
     private Touch touch;
-    private float speedModifier;
+    private Vector3 touchStartPosition;
+    private Vector2 differenceFromBeginning;
+
+    private Vector3 unclampedVelocity;
+    #endregion
     
+    #region LifecycleMethods
     //start is called before the first frame update
     void Start()
     {
-        speedModifier = 0.1f;
-        Debug.Log("here 2");
+        rigidbody = GetComponent<Rigidbody>();
     }
             
     // Update is called once per frame
     void Update()
     {
+        ReceiveTouchInputs();
+    }
+    
+    private void FixedUpdate()
+    {
+        SetPlayerVelocity();
+    }
+    #endregion
+    
+    #region OtherMethods
+    private void ReceiveTouchInputs()
+    {
         if (Input.touchCount <= 0) return;
         
-        Debug.Log("here 3");
-
         touch = Input.GetTouch(0);
-        
-        Debug.Log("here 4");
 
-        
-        if (touch.phase != TouchPhase.Moved) return;
+        if (touch.phase == TouchPhase.Began)
+        {
+            touchStartPosition = touch.position;
+        }
 
-        Debug.Log("here 1");
-        
-        transform.position = new Vector3(
-            transform.position.x + touch.deltaPosition.x * speedModifier
-            , transform.position.y
-            ,transform.position.z + touch.deltaPosition.y * speedModifier
-        );
+        if (touch.phase == TouchPhase.Stationary)
+        {
+            differenceFromBeginning.x = touch.position.x - touchStartPosition.x;
+            differenceFromBeginning.y = touch.position.y - touchStartPosition.y;
+            
+            unclampedVelocity.x = differenceFromBeginning.x * speedModifier;
+            unclampedVelocity.z = differenceFromBeginning.y * speedModifier;
+        }
     }
+
+    private void SetPlayerVelocity()
+    {
+        rigidbody.velocity = unclampedVelocity.normalized * maxVelocity;
+        
+        if (!(rigidbody.velocity.magnitude > 0.01f)) return;
+        
+        transform.rotation = Quaternion.LookRotation(rigidbody.velocity, Vector3.up);
+    }
+    #endregion
 }
