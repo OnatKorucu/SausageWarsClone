952a1098fb9238847f521af3ebbf3ef5ba42fe5a
OnatKorucu <onatkorucu@gmail.com>
2022-04-29 11:01:22 +0300

    fix: collectable extra spawn fixed.

--- Assets/Scripts/CollectableSpawner.cs
+++ Assets/Scripts/CollectableSpawner.cs
@@ -8,8 +8,12 @@ using Random = UnityEngine.Random;
 public class CollectableSpawner : MonoBehaviour
 {
     #region Variables
+    [SerializeField] private List<GameObject> collectableTypes;
+
     [SerializeField] private GameObject spawnArea;
-    //[SerializeField] private Vector3 spawnOffset;//TODO: Clean
+    private Vector3 center;
+    private Vector3 size;
+    
     [SerializeField, Range(1, 10)] private int startingCollectableCount;
     [SerializeField, Range(1, 20)] private int maximumCollectableCount;
     private volatile int collectableCountInGame;
@@ -18,42 +22,25 @@ public class CollectableSpawner : MonoBehaviour
     [SerializeField, Range(0, Mathf.Infinity)] private float randomSpawnTimeOffset;
     private float nextSpawnTime;
     private float spawnTimeCounter;
-    
-    [SerializeField] private List<GameObject> collectableTypes;
-    
-    private Vector3 center;
-    private Vector3 size;
     #endregion
 
     #region LifecycleMethods
-    private void OnEnable()
-    {
-        BaseCollectable.OnCollected += HandleOnCollected;
-    }
-
-    private void OnDisable()
-    {
-        BaseCollectable.OnCollected -= HandleOnCollected;
-    }
-    
     // Start is called before the first frame update
     void Start()
     {
         ClampRandomSpawnTimeOffset();
 
         InitializeCenterAndSize();
-
-        SpawnInitialCollectables();
-
+        
         CalculateNextSpawnTime();
     }
-
+    
     private void LateUpdate()
     {
         spawnTimeCounter += Time.deltaTime;
-
+        
         if (collectableCountInGame >= maximumCollectableCount) { return; }
-
+        
         if (spawnTimeCounter < nextSpawnTime) { return; }
         
         GameObject collectableToSpawn = PickRandomCollectable();
@@ -64,14 +51,16 @@ public class CollectableSpawner : MonoBehaviour
     #endregion
 
     #region OtherMethods
-    private void HandleOnCollected()
+    public void HandleOnCollected()
     {
-        Debug.Log("xxx HandleOnCollected");
-
         collectableCountInGame--;
         
+        /*if (collectableCountInGame < 0)
+        {
+            collectableCountInGame = 0;
+        }*/
+        
         Debug.Log("xxx collectableCountInGame is: " + collectableCountInGame);
-
     }
 
     private void ClampRandomSpawnTimeOffset()
@@ -87,30 +76,19 @@ public class CollectableSpawner : MonoBehaviour
         center = new Vector3(spawnArea.transform.position.x, spawnArea.transform.position.y, spawnArea.transform.position.z);
         size = spawnArea.GetComponent<Renderer>().bounds.size;
     }
-    
-    private void SpawnInitialCollectables()
-    {
-        for (int i = 0; i < startingCollectableCount; i++)
-        {
-            GameObject collectableToSpawn = PickRandomCollectable();
-            SpawnCollectable(collectableToSpawn);
-        }
-    }
-    
+
     private void CalculateNextSpawnTime()
     {
-        nextSpawnTime = baseSpawnInterval + (Random.value - 0.5f) * randomSpawnTimeOffset;
-        Debug.Log("NextSpawnTime: " + nextSpawnTime);
-    }
-
-    private GameObject PickRandomCollectable()
-    {
-        return collectableTypes.RandomElement();
+        nextSpawnTime = baseSpawnInterval + (Random.value - 0.5f) * randomSpawnTimeOffset; //TODO
     }
 
     private void SpawnCollectable(GameObject collectableToSpawn)
     {
-        if (collectableCountInGame >= maximumCollectableCount) { return; }
+        if (collectableCountInGame >= maximumCollectableCount)
+        {
+            //collectableCountInGame = maximumCollectableCount;
+            return;
+        }
 
         GameObject collectableGameObject = Instantiate(collectableToSpawn);
         Debug.Log("xxx SPAWN");
@@ -119,8 +97,18 @@ public class CollectableSpawner : MonoBehaviour
         collectableGameObject.transform.rotation = Quaternion.identity;
 
         collectableCountInGame++;
-        Debug.Log("xxx collectableCountInGame is: " + collectableCountInGame);
 
+        if (collectableCountInGame >= maximumCollectableCount)
+        {
+            //collectableCountInGame = maximumCollectableCount;
+        }
+
+        Debug.Log("xxx collectableCountInGame is: " + collectableCountInGame);
+    }
+    
+    private GameObject PickRandomCollectable()
+    {
+        return collectableTypes.RandomElement();
     }
 
     private Vector3 PickSpawnPosition(Vector3 center, Vector3 size)//TODO: Clean
